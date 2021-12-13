aed9257fc55b1480d7da45aadfaf498dab624103
Nacho-JB15 <85852842+Nacho-JB15@users.noreply.github.com>
2021-12-13 17:04:31 -0300

    Initial commit

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  headers: HttpHeaders;
+
+  constructor(public http: HttpClient
+) {
+    this.headers = new HttpHeaders();
+    this.headers.append("Accept", 'application/json');
+    this.headers.append('Content-Type','application/json');
+    this.headers.append('Access-Control-Allow-Origin','*');
+   }
+
+  addStudent(data){
+    return this.http.post('http://localhost/ionic-crud-nacho/backend/create.php',data);
+  }
+}
+ 
+  
